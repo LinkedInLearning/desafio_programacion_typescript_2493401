2655144f6cf24c5cf63113673114ee1c5d9347b8
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-09-28 04:14:05 +0100

    desafio_05_i

---
+++ desafio_05/inicio.ts
@@ -0,0 +1,13 @@
+
+//Implementa la función aquí:
+
+
+
+const resultado1 = contieneDuplicados([1, 2, 3, 4, 6, 5, 6, 7]);
+console.log(resultado1);
+
+const resultado2 = contieneDuplicados([{a: 1}, {b: 2}, {a: 1}]);
+console.log(resultado2);
+
+const resultado3 = contieneDuplicados(['casa', 'zebra', 'bosque', 'luna']);
+console.log(resultado3);
