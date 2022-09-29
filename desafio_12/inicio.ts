f7e5f43d73bcd15db5de7b97569045dfc85f7b83
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-09-29 03:18:36 +0100

    amend

--- desafio_12/inicio.ts
+++ desafio_12/inicio.ts
@@ -1,12 +1,9 @@
-
-function sonIguales(valor1: unknown, valor2: unknown): boolean{
-    
+interface Productos {
+    arroz: {enInventario: true},
+    azucar: {enInventario: false},
+    carne: {enInventario: true},
+    leche: {enInventario: false},
 }
 
-console.log(sonIguales('a', 'b'));//false
-console.log(sonIguales([1], [1]));//true
-console.log(sonIguales([1], [2]));//false
-console.log(sonIguales({a: 1}, {a: 1}));//true
-console.log(sonIguales({a: 1, b: 2, c: 3}, {a: 1, b: 2, c: 5}));//false
-console.log(sonIguales({a: 1, b: ['a', 'b'], c: 3}, {a: 1, b: ['a', 'b'], c: 3}));//true
-console.log(sonIguales({a: 1, b: ['a', 'b'], c: 3}, {a: 1, b: ['a', 'c'], c: 3}));//false
+
+type ProductosEnInventario = ?;
