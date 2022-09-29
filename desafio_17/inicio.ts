54436043fc8c327e045e617d87b4306e61733c28
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-09-30 00:36:15 +0100

    desafio_17_i

---
+++ desafio_17/inicio.ts
@@ -0,0 +1,14 @@
+
+
+
+const prueba = clousure(10);
+console.log(prueba());
+console.log(prueba());
+console.log(prueba());
+console.log(prueba());
+
+const pruebaArreglo = clousure([10, 20, 30, 40]);
+console.log(pruebaArreglo());
+console.log(pruebaArreglo());
+console.log(pruebaArreglo());
+console.log(pruebaArreglo());
