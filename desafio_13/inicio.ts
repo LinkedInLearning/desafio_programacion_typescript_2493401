33fd85e8f5780dd579faee32da62d14292f2922d
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-09-29 02:40:20 +0100

    desafio_13_i

---
+++ desafio_13/inicio.ts
@@ -0,0 +1,27 @@
+const eventosJavaScript = [
+    {
+        nombre: 'Evento click',
+        evento: 'onclick',
+    },
+   {
+        nombre: 'Evento de cambio de un elemento',
+        evento: 'onchange',
+    },
+    {
+        nombre: 'Evento cuando el puntero se mueve dentro del área de un elemento',
+        evento: 'onmouseover',
+    }, 
+    {
+        nombre: 'Evento cuando el puntero sale del del área de un elemento',
+        evento: 'onmouseout',
+    },
+    {
+        nombre: 'Evento cuando el usuario presiona una tecla',
+        evento: 'onkeydown',
+    },
+    {
+        nombre: 'Evento cuando la página termina la carga',
+        evento: 'onload',
+    }, 
+];
+
