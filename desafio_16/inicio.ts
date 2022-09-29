d647dc060f403e791c855a3d944ed74f1d5f8c82
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-09-29 02:43:23 +0100

    desafio_16_i

---
+++ desafio_16/inicio.ts
@@ -0,0 +1,24 @@
+
+
+
+imprimirValores([1,2,3,4]);
+
+const iterable = {
+    [Symbol.iterator]: function* () {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+    }
+};
+
+imprimirValores(iterable);
+
+const mapa = new Map<number, string>([
+    [1, 'prueba1'],
+    [2, 'prueba2'],
+    [3, 'prueba3'],
+    [4, 'prueba4'],
+]);
+
+imprimirValores(mapa);
