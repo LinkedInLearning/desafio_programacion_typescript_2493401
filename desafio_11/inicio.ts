17b38b715bfce1330a9224fc747b9011c454b6fe
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-01 23:31:22 +0100

    desafio_11_i

---
+++ desafio_11/inicio.ts
@@ -0,0 +1,8 @@
+type ListaProductos = 'fresas' | 'arroz' | 'frijoles' | 'tomates' | 'pollo' | 'bistec' | 'mandarinas';
+
+type Propiedades = {
+    cantidad: number,
+    precio: number,
+    categoria: string,
+    tipo: string,
+};
