b999301510dd41be90c98480eb0127a3d7b59331
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-01 23:37:43 +0100

    amend

--- desafio_05/inicio.ts
+++ desafio_05/inicio.ts
@@ -6,7 +6,7 @@
 const resultado1 = contieneDuplicados([1, 2, 3, 4, 6, 5, 6, 7]);
 console.log(resultado1);
 
-const resultado2 = contieneDuplicados([{a: 1}, {b: 2}, {a: 1}]);
+const resultado2 = contieneDuplicados([true, false, true]);
 console.log(resultado2);
 
 const resultado3 = contieneDuplicados(['casa', 'zebra', 'bosque', 'luna']);
