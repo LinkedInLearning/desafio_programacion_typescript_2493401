df521ed954a0d4c4153065692d072aa36c0d688b
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-05 19:39:47 +0100

    01_f

---
+++ desafio_01/final.ts
@@ -0,0 +1,24 @@
+
+interface Perfil{
+    nombre: string;
+    compania: string;
+    fechaNacimiento: Date;
+    direccion: string;
+    acerca: string;
+}
+
+interface Acceso {
+    id: string;
+    email: string;
+    usuario: string;
+    contrasena: string;
+}
+
+type Usuario = Perfil & Acceso;
+
+function crearUsuario(perfil: Perfil, acceso: Acceso): Usuario {
+    return {
+        ...perfil,
+        ...acceso,
+    }
+}
