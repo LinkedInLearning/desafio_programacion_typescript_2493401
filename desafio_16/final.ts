5c13886caf539b3a8eac8ef98f6418bedf0d36f2
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-05 20:34:25 +0100

    16_F

---
+++ desafio_16/final.ts
@@ -0,0 +1,30 @@
+function mapearValores<T, A>(lista: Iterable<T>, mapeo: (valor: T) => A): Array<A>{
+    return Array.from(lista, mapeo);
+}
+
+mapearValores([1,2,3,4], num => num * 2);
+
+
+interface IterateNumber {
+  [Symbol.iterator](): IterableIterator<number>;
+}
+
+const iterable: IterateNumber = {
+    [Symbol.iterator]: function* () {
+        yield 1;
+        yield 2;
+        yield 3;
+        yield 4;
+    }
+};
+
+mapearValores(iterable, num => num * 2);
+
+const mapa = new Map<number, string>([
+    [1, 'prueba1'],
+    [2, 'prueba2'],
+    [3, 'prueba3'],
+    [4, 'prueba4'],
+]);
+
+mapearValores<[number, string], string>(mapa, (val: [number, string] ) => val[1]);
