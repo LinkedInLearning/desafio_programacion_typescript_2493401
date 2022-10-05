1d6e97aba6cefd5d2ad79691aa2031e37f693de9
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-10-05 19:43:49 +0100

    04_f

---
+++ desafio_04/final.ts
@@ -0,0 +1,31 @@
+type TipoArchivo = 'XML' | 'TXT' | 'JSON';
+type Acciones = 'Crear' | 'Actualizar' | 'Borrar';
+type EventoArchivo = `${Acciones}${TipoArchivo}`;
+
+function procesarDatos(evento: EventoArchivo, datos: string): void{
+    console.log(evento);
+    console.log(datos);
+}
+
+procesarDatos('CrearXML',
+`<modulo numero="10">
+  <nombre>Lorem ipsum dolor</nombre>
+  <descripcion>Lorem ipsum dolor sit amet,
+  consectetur adipiscing elit.
+  Cras malesuada libero vel dui dapibus,
+  a molestie augue semper.
+  </descripcion>
+</modulo>`);
+
+procesarDatos('ActualizarJSON', 
+`"perfil": {
+        "nombre": "Caitlin Pugh",
+        "compania": "Zillar",
+        "f_nacimiento": "1989-07-20",
+        "direccion": "51 Eckford Street, Salvo, Delaware",
+        "ubicacion": {
+          "lat": 50.508825,
+          "long": -121.813101
+        },
+        "acerca": "Reprehenderit incididunt laborum irure enim aliqua pariatur esse excepteur culpa. Eu non Lorem fugiat eiusmod fugiat."
+}`);
